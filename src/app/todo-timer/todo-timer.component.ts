1413b2d0599c23fd8e1cf3a83ffc6b35d60a1bb3
Bruno Cardoso <bruno.testahy.cardoso@celfocus.com>
2018-12-06 22:41:01 +0000

    core for timer with observable implemented

--- src/app/todo-timer/todo-timer.component.ts
+++ src/app/todo-timer/todo-timer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { concat, NEVER, Subject, timer } from 'rxjs';
+import { scan, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-todo-timer',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-timer.component.scss']
 })
 export class TodoTimerComponent implements OnInit {
+  private start$ = new Subject();
+  private stop$ = new Subject();
 
   constructor() { }
 
   ngOnInit() {
+    this.buildTimer();
   }
 
+  public play() {
+    this.start$.next(false);
+  }
+
+  public pause() {
+    this.start$.next(true);
+  }
+
+  private buildTimer(): void {
+    concat(this.start$, this.stop$)
+      .pipe(
+        switchMap(stopped => (stopped ? NEVER : timer(0, 1000))),
+        scan(acc => acc + 1, 0),
+      )
+      .subscribe(console.log);
+
+    this.start$.next(false);
+  }
 }
