a28583c9a466a9fb29d82f78fa0d46064c9d6f24
Bruno Cardoso <bruno.testahy.cardoso@celfocus.com>
2018-12-06 21:33:41 +0000

    create todo-timer component

---
+++ src/app/todo-timer/todo-timer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-timer',
+  templateUrl: './todo-timer.component.html',
+  styleUrls: ['./todo-timer.component.scss']
+})
+export class TodoTimerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
