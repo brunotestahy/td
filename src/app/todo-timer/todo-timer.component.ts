f92251db382ea0704d7fe6bf64a36a1da1b4c2d3
Bruno Cardoso <bruno.testahy.cardoso@celfocus.com>
2018-12-07 01:25:02 +0000

    implement logic for timer adjust layout

--- src/app/todo-timer/todo-timer.component.ts
+++ src/app/todo-timer/todo-timer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { concat, NEVER, Subject, timer } from 'rxjs';
 import { scan, switchMap } from 'rxjs/operators';
 
@@ -11,8 +11,12 @@ export class TodoTimerComponent implements OnInit {
   private start$ = new Subject();
   private stop$ = new Subject();
   public isStopped = false;
+  private MAX_TIME =  1200; // 30 minutes in seconds
+  borderD = '';
+  loaderD = '';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
     this.buildTimer();
@@ -26,11 +30,42 @@ export class TodoTimerComponent implements OnInit {
   private buildTimer(): void {
     concat(this.start$, this.stop$)
       .pipe(
-        switchMap(stopped => (stopped ? NEVER : timer(0, 1000))),
-        scan(acc => acc + 1, 0),
+        switchMap(stopped => (stopped ? NEVER : timer(0, 100))),
+        scan(acc => acc - 1, this.MAX_TIME),
       )
-      .subscribe(console.log);
+      .subscribe(this.updateLoader.bind(this));
 
     this.start$.next(this.isStopped);
+  }
+
+  private updateLoader(currentValue: number) {
+    currentValue = this.MAX_TIME - currentValue;
+    const PI = Math.PI;
+    const totalSections = 360;
+    const semiSections = totalSections / 2;
+    console.log(currentValue);
+
+    currentValue %= totalSections;
+    const r = (currentValue * PI / semiSections);
+    const x = Math.sin(r) * 125;
+    const y = Math.cos(r) * - 125;
+    const mid = (currentValue > semiSections) ? 1 : 0;
+    const anim = 'M 0 0 v -125 A 125 125 1 ' + mid + ' 1 ' + x + ' ' + y + ' z';
+
+    if (r === 0) {
+      this.toggleExecution();
+      return;
+    }
+
+    this.borderD = anim;
+    this.loaderD = anim;
+
+    this.checkMaxTime(currentValue);
+  }
+
+  private checkMaxTime(seconds: number) {
+    if (seconds === this.MAX_TIME - 1) {
+      this.toggleExecution();
+    }
   }
 }
